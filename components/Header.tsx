165c5398edf9f059836b69f7a0ba043d8382a43b
AayushMohan <aayushmohan1702@gmail.com>
2022-03-30 01:08:54 +0530

    Added AirBnb Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header>
+    <header className="sticky top-0 z-50">
       {/* Left */}
-      <div></div>
+      <div className="relative my-auto flex h-10 w-10 cursor-pointer items-center">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
 
       {/* Center */}
       <div></div>
