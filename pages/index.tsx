d4c41b9c900b371e4d26753fe386e946a5fb8a97
AayushMohan <aayushmohan1702@gmail.com>
2022-03-31 02:11:42 +0530

    Added Static Props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -18,6 +17,11 @@ const Home: NextPage = () => {
       <main className="mx-auto max-w-7xl px-8 sm:px-16">
         <section className="pt-6">
           <h2 className="pb-5 text-4xl font-semibold">Explore Nearby</h2>
+
+          {/* Pull some data from a server - API endpoints */}
+          {exploreData?.map((item: any) => (
+            <h1>{item.location}</h1>
+          ))}
         </section>
       </main>
     </div>
@@ -25,3 +29,13 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getStaticProps() {
+  const exploreData = await fetch('https://links.papareact.com/pyp').then(
+    (res) => res.json()
+  )
+
+  return {
+    props: { exploreData: exploreData },
+  }
+}
