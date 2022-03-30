381f76bcc7a330d80ec2a4b94b5e8b64ca8dbc48
AayushMohan <aayushmohan1702@gmail.com>
2022-03-30 21:07:13 +0530

    Added Hero Icons

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white py-5 shadow-md">
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white py-5 shadow-md md:px-10">
       {/* Left */}
       <div className="relative my-auto flex h-10 cursor-pointer items-center">
         <Image
@@ -15,8 +15,10 @@ const Header = () => {
         />
       </div>
 
-      {/* Center */}
-      <div></div>
+      {/* Middle - Search */}
+      <div>
+        <input type="text" />
+      </div>
 
       {/* Right */}
       <div></div>
